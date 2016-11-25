6d2f6f2040f384f077b872bd8ecfdee603b5708d
Nick Landry <nlandry@activenick.net>
2016-11-24 22:41:21 -0500

    (mostly) working animations on Robot Kyle

---
+++ Assets/LocomotionSMB.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocomotionSMB : StateMachineBehaviour {
+
+    public float m_Damping = 0.15f;
+
+    private readonly int m_HashHorizontalPara = Animator.StringToHash("Horizontal");
+    private readonly int m_HashVerticalPara = Animator.StringToHash("Vertical");
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 input = new Vector2(horizontal, vertical).normalized;
+
+        animator.SetFloat(m_HashHorizontalPara, input.x, m_Damping, Time.deltaTime);
+        animator.SetFloat(m_HashVerticalPara, input.y, m_Damping, Time.deltaTime);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
